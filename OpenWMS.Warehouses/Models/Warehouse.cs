b416f6bdba7725f4d24c1ce5ac79b3053e39c3f8
Fabio Nascimento <fabio.o.nascimento@gmail.com>
2012-06-30 17:36:02 -0300

    Add Tests for Warehouses

---
+++ OpenWMS.Warehouses/Models/Warehouse.cs
@@ -0,0 +1,22 @@
+﻿using OpenWMS.Warehouses.Interfaces;
+
+namespace OpenWMS.Warehouses.Models
+{
+    public class Warehouse : IWarehouse
+    {
+        public static IWarehouse New()
+        {
+            return new Warehouse();
+        }
+
+        public IWarehouse SetId(string newId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IWarehouse SetName(string newName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- OpenWMS.Warehouses/Tests/WarehousesTests.cs
+++ OpenWMS.Warehouses/Tests/WarehousesTests.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+using OpenWMS.Warehouses.Models;
 
 namespace OpenWMS.Warehouses.Tests
 {
 
+    [TestFixture]
     public class WarehousesTests
     {
-
+        [Test]
         public void Posso_Controlar_Um_Deposito()
         {
-            
+            var warehouse = Warehouse.
+                New();
+
+            warehouse.Should().NotBeNull();
         }
     }
 }
