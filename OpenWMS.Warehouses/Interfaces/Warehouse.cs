05fe8be3ab3be64256f26fcab6f2fab67b1a5377
Fabio Nascimento <fabio.o.nascimento@gmail.com>
2012-06-30 17:24:40 -0300

    Add NUnit Package to Warehouses]

---
+++ OpenWMS.Warehouses/Interfaces/Warehouse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenWMS.Warehouses.Interfaces
+{
+    public interface IWarehouse
+    {
+        IWarehouse SetId(string newId);
+        IWarehouse SetName(string newName);        
+    }
+}
---
+++ OpenWMS.Warehouses/Tests/WarehousesTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenWMS.Warehouses.Tests
+{
+
+    public class WarehousesTests
+    {
+
+        public void Posso_Controlar_Um_Deposito()
+        {
+            
+        }
+    }
+}
